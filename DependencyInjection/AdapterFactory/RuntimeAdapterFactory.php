d1e17147f1905ee16b04e6b1e0555bec36c7344c
Benjamin Dulau <benjamin.dulau@anonymation.com>
2013-03-20 17:30:48 +0100

    Added RuntimAdapterFactory

---
+++ DependencyInjection/AdapterFactory/RuntimeAdapterFactory.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of AnoBarbeQBundle
+ *
+ * (c) anonymation <contact@anonymation.com>
+ *
+ */
+namespace Ano\Bundle\BarbeQBundle\DependencyInjection\AdapterFactory;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
+
+/**
+ * Factory for Runtime adapter
+ *
+ * @author Benjamin Dulau <benjamin.dulau@gmail.com>
+ */
+class RuntimeAdapterFactory implements AdapterFactoryInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function create(ContainerBuilder $container, $id, array $config)
+    {
+        $container
+            ->setDefinition($id, new DefinitionDecorator('ano_barbeq.adapter.runtime'))
+        ;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getKey()
+    {
+        return 'runtime';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function addConfiguration(ArrayNodeDefinition $node)
+    {
+        // no configuration
+        $node
+            ->children()
+            ->end()
+        ;
+    }
+}
